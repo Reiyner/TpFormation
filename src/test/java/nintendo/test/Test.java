23d08a92571694a7a72215b27fa3625a95116917
reiyner <maxence.palkowski1@gmail.com>
2025-03-20 17:50:36 +0100

    Création de jeux dans le main

--- src/test/java/nintendo/test/Test.java
+++ src/test/java/nintendo/test/Test.java
@@ -1,9 +1,25 @@
 package nintendo.test;
 
+
+
+import nintendo.model.Console;
+import nintendo.model.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Console nintendoSwitch = new Console("switch");
+		Console playstationV = new Console("playstationV");
+		Console xbox = new Console("xbox");
+		Console nintendoDS = new Console("nintendoDS");
+		Console pc  = new Console("pc");
+
+		Jeu hades = new Jeu("Hades",playstationV);
+		Jeu	zelda = new Jeu("Zelda",nintendoSwitch);
+		Jeu	pokemon = new Jeu("Pokemon",nintendoDS);
+		Jeu civilisation = new Jeu("Civilisation",pc);
+		Jeu titanFall = new Jeu("Titan Fall",xbox);
 
 	}
 
