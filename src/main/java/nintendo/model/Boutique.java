02a21623666ce32b18d62e229d5ff745d9b94f43
gbaillot <gbaillot26@gmail.com>
2025-03-20 17:28:33 +0100

    Creation Class Boutique

---
+++ src/main/java/nintendo/model/Boutique.java
@@ -0,0 +1,33 @@
+
+public class Boutique {
+	private String nom;
+	private String Adresse;
+	
+	public Boutique(String nom, String adresse) {
+		this.nom = nom;
+		Adresse = adresse;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return Adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
+	}
+
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + ", Adresse=" + Adresse + "]";
+	}
+	
+	
+}
