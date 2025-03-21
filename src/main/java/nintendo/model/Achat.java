c335da7f65853ddc4c88b4b9e241615fb4334448
reiyner <maxence.palkowski1@gmail.com>
2025-03-21 10:20:30 +0100

    add a toString to achat class

--- src/main/java/nintendo/model/Achat.java
+++ src/main/java/nintendo/model/Achat.java
@@ -12,6 +12,11 @@ public class Achat {
 		this.date = date;
 		this.prix = prix;
 	}
+
+	@Override
+	public String toString() {
+		return "Achat [jeu=" + jeu + ", date=" + date + ", prix=" + prix + "]";
+	}
 	
 
 }
